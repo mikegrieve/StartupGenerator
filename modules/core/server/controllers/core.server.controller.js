e8e653ccf107615f3bbc732ef3cf5764bd19cccd
MATT GARNETT <MATT@MATTGARNETT.COM>
2018-01-23 19:32:55 -0800

    finished

--- modules/core/server/controllers/core.server.controller.js
+++ modules/core/server/controllers/core.server.controller.js
@@ -8,26 +8,16 @@ var validator = require('validator'),
  * Render the main application page
  */
 exports.renderIndex = function (req, res) {
-  var safeUserObject = null;
-  if (req.user) {
-    safeUserObject = {
-      displayName: validator.escape(req.user.displayName),
-      provider: validator.escape(req.user.provider),
-      username: validator.escape(req.user.username),
-      created: req.user.created.toString(),
-      roles: req.user.roles,
-      profileImageURL: req.user.profileImageURL,
-      email: validator.escape(req.user.email),
-      lastName: validator.escape(req.user.lastName),
-      firstName: validator.escape(req.user.firstName),
-      additionalProvidersData: req.user.additionalProvidersData
-    };
-  }
 
-  res.render('modules/core/server/views/index', {
-    user: JSON.stringify(safeUserObject),
-    sharedConfig: JSON.stringify(config.shared)
-  });
+  var unicorns = ["Uber","Airbnb","Pinterest","Dropbox","Snapchat","Tinder","Yelp","Kik","YouTube","Twitter","PornHub" ];
+  var nouns = ["Albinos", "Wizards", "Assholes", "Alcoholics","Old People", "Mutes", "Nerds","Racists","Pedophiles","Goths", "Girls", "Politicians", "Blondes", "Teens", "Anorexic People"];
+  var icons = ["fa-user-circle-o", "fa-ravelry", "fa-telegram", "fa-wpexplorer", "fa-binoculars", "fa-book", "fa-camera-retro", "fa-bug", "fa-certificate", "fa-clone", "fa-cubes", "fa-hand-pointer-o", "fa-handshake-o", "fa-heart-o", "fa-credit-card", "fa-btc", "fa-align-justify", "fa-arrow-circle-right", "fa-gg"];
+  var uRand = Math.floor(Math.random() * unicorns.length);
+  var nRand = Math.floor(Math.random() * nouns.length);
+  var iRand = Math.floor(Math.random() * icons.length);
+
+  res.render("modules/core/server/views/hello", { unicorn: unicorns[uRand], noun: nouns[nRand], icon: icons[iRand]  } );
+
 };
 
 /**
